16505b117660bbacd2beef6b518092c20cd7217c
ricaesar <41854090+ricaesar@users.noreply.github.com>
2026-03-18 07:53:52 +0700

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,20 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "hello dari vercel" });
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://be-lms.kemkes.go.id/v1/ext/fasynakes/course?page=1&pageSize=10",
+      {
+        headers: {
+          "x-api-key": "ISI_API_KEY_KAMU",
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({
+      error: error.toString(),
+    });
+  }
 }
